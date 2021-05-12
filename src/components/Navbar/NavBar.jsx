ccac880f0d62593ba456837ff9cc98d2ff5ca1e7
Ankita Panjwani <ankitapan@cybage.com>
2021-05-12 11:56:48 +0530

    nav ank

--- src/components/Navbar/NavBar.jsx
+++ src/components/Navbar/NavBar.jsx
@@ -1,5 +1,5 @@
 // Navbar
-import React from 'react';
+import React, { useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
@@ -9,44 +9,26 @@ import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
 
 
-const useStyles = makeStyles((theme) => ({
-    root: {
-        flexGrow: 1,
-    },
-    // menuButton: {
-    //     marginRight: theme.spacing(2),
-    // },
-    title: {
-        flexGrow: 1,
-    },
-    AppColor: {
-        backgroundColor: '#333333'
-    },
-    btnhover:{
-        borderRadius: '20px',
-        width: '100px',
-        padding:'5px',
-        // backgroundColor: 'red',
-        "&:hover":{
-            borderRadius: '20px',
-            width: '100px',
-            padding:'5px',
-            backgroundColor: '#595757'
-        }
-    }
-}));
+import { useStyles } from './Navbar.style';
+
 function NavBar() {
     const classes = useStyles();
+    const [menuState, setMenuState] = useState(false)
     return (
         <div>
             <AppBar position="fixed" className={classes.AppColor}>
                 <Toolbar>
-                    {/* <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-                        <MenuIcon />
-                    </IconButton> */}
+                    {
+                        menuState
+                            ? <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+                                <MenuIcon />
+                            </IconButton>
+                            : <></>
+                    }
                     <Typography variant="h6" className={classes.title}>
-                        CpSmartTech
+                        Cpsmarttech
                     </Typography>
+                    
                     <Button color="inherit" className={classes.btnhover}>Home</Button>
                     <Button color="inherit" className={classes.btnhover}>About</Button>
                     <Button color="inherit" className={classes.btnhover}>Team</Button>
--- src/components/Navbar/Navbar.style.jsx
+++ src/components/Navbar/Navbar.style.jsx
@@ -1,1 +1,31 @@
 // Navbar Style
+import { makeStyles} from "@material-ui/core/styles";
+export const useStyles = makeStyles((theme)=>({
+    root: {
+        flexGrow: 1,
+    },
+    // menuButton: {
+    //     marginRight: theme.spacing(2),
+    // },
+    title: {
+        flexGrow: 1,
+    },
+    AppColor: {
+        backgroundColor: '#333333'
+    },
+   
+
+    btnhover: {
+        borderRadius: '20px',
+        width: '100px',
+        padding: '5px',
+        // backgroundColor: 'red',
+        "&:hover": {
+            borderRadius: '20px',
+            width: '100px',
+            padding: '5px',
+            color: '#333333',
+            backgroundColor: 'white'
+        }
+    }
+}))
